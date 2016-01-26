31ace15da36f82eea2029b82de44bb445cf99894
jeanne27king <jeanne27king@att.net>
2016-01-26 09:06:43 -0500

    Rev2

--- Lab1canVoteSolution/Lab1canVote/Program.cs
+++ Lab1canVoteSolution/Lab1canVote/Program.cs
@@ -19,7 +19,6 @@
             // | start collecting the data.                                                       |
             // +==================================================================================+
 
-
             System.Console.Write("What is your first name? ");
             firstName = System.Console.ReadLine();
 
@@ -29,31 +28,37 @@
             System.Console.Write("What is your last name? ");
             lastName = System.Console.ReadLine();
 
-            //fullName = firstName + " " + middleInitial + ". " + lastName
+            fullName = (firstName + " " + middleInitial + ". " + lastName);
 
-            System.Console.Write("How tall are you? (feet only... 4, 5, 6)? ");
+            System.Console.Write("How tall are you? feet only 4, 5, 6? ");
             heightFeet = int.Parse(System.Console.ReadLine());
 
             System.Console.Write("How many inches added to the feet are you? ");
-            heightInches = System.Console.ReadLine();
+            heightInches = double.Parse(System.Console.ReadLine());
 
-            //totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
+            totalHeightCM = ((heightFeet * 12) + heightInches) * 2.54;
 
             System.Console.Write("How old are you? ");
             age = int.Parse(System.Console.ReadLine());
 
             System.Console.Write("Are you a U.S. citizen? ");
-            isCitizen = System.Console.ReadLine();
+            isCitizen = bool.Parse(System.Console.ReadLine());
 
-            //canVote = (age >= 18) + isCitizen;
-            
+            canVote = (age >= 18) == isCitizen;
+
             // +==================================================================================+
-            // | Write the information to the screen.   R2        |
+            // | Write the information to the screen.   R2                                        |
             // +==================================================================================+
 
-            //System.Console.Write fullName;
-            //System.Console.Write totalHeightCM +" centimeters";
-            //System.Console.Write canVote
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+            System.Console.Write(fullName);
+            System.Console.WriteLine();
+            System.Console.Write(totalHeightCM +" centimeters");
+            System.Console.WriteLine();
+            System.Console.Write(canVote);
+            System.Console.WriteLine();
+            System.Console.ReadKey();
             }
     }
 }
