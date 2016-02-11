c66217047ab88c99c5f25a0c78463da854a86d2f
jeanne27king <jeanne27king@att.net>
2016-02-11 09:30:20 -0500

    spouse changes

--- Lab2Solution/Lab2Person/Person.cs
+++ Lab2Solution/Lab2Person/Person.cs
@@ -6,8 +6,8 @@
         public int Age = 0;
         public string firstName;
         public string lastName;
-        public string spouseName;
         public Person Spouse;
+        public static double SumOfAllAges = 0;
 
         public void InputPersonData()        //invoke method input person firstName, LastName, and age?
         {
@@ -17,29 +17,30 @@
             this.lastName = System.Console.ReadLine();
             System.Console.Write("What is your age? ");
             this.Age = int.Parse(System.Console.ReadLine());
-            Age++;
+            SumOfAllAges += this.Age;
         }
 
-        public void SpouseInputData()  //invoke method input spouse name and age
+        public void SpouseInputData()  //invoke method input spouse name and age and set last name
         {
+            this.Spouse = new Person();
             System.Console.Write("What is your spouse's first name? ");
-            this.firstName = System.Console.ReadLine();
+            this.Spouse.firstName = System.Console.ReadLine();
             System.Console.Write("What is your spouse's age? ");
-            this.Age = int.Parse(System.Console.ReadLine());
-            Age++;     
+            this.Spouse.Age = int.Parse(System.Console.ReadLine());
+            this.Spouse.lastName = this.lastName;
+            this.Spouse.Spouse = this;
+            SumOfAllAges += this.Spouse.Age;     
+        }
+
+        public string GetFullName()
+        { 
+            return this.firstName + " " + this.lastName; 
         }
 
         public void PrintNameandAge()   //invoke print name and age?
         {
-            System.Console.WriteLine(this.firstName + " " + this.lastName + " is " + this.Age + " years old.");
+            System.Console.WriteLine(this.GetFullName() + " is " + this.Age + " years old.");
+            System.Console.WriteLine(this.Spouse.GetFullName() + " is " + this.Spouse.Age + " years old.");
         }
-
-
-        //public static int SumOfAllAges();
-        //    return Age/4;
-
-
-            
-
     }
 }
--- Lab2Solution/Lab2Person/Program.cs
+++ Lab2Solution/Lab2Person/Program.cs
@@ -6,21 +6,14 @@
         {
             bool maritalStatus;
 
-
-            //Person 1 
             Person p1 = new Person();
             System.Console.Write("Person 1: ");
-            //input person firstName, LastName, and age?
-            p1.InputPersonData();
+            p1.InputPersonData();  
+
             System.Console.Write("True or False... Are you married? ");
             maritalStatus = bool.Parse(System.Console.ReadLine());
             //if marital status is true then create new spouse?
-            //input spouse firstName, set LastName, and age?
-            Person p1spouse = new Person();
-            p1spouse.SpouseInputData();
-            p1spouse.lastName = p1.lastName;
-            p1.spouseName = p1spouse.firstName;
-            p1spouse.spouseName = p1.firstName;
+            p1.SpouseInputData();
             System.Console.WriteLine();
 
 
@@ -33,22 +26,17 @@
             maritalStatus = bool.Parse(System.Console.ReadLine());
             //if marital status is true then create new spouse?
             //input spouse firstName, set LastName, and age?
-            Person p2spouse = new Person();
-            p2spouse.SpouseInputData();
-            p2spouse.lastName = p2.lastName;
-            p2.spouseName = p2spouse.firstName;
-            p2spouse.spouseName = p2.firstName;
+            p2.SpouseInputData();
             System.Console.WriteLine();
 
             p1.PrintNameandAge();
-            p1spouse.PrintNameandAge();
+            //p1spouse.PrintNameandAge();
             p2.PrintNameandAge();
-            p2spouse.PrintNameandAge();
+            //p2spouse.PrintNameandAge();
+            System.Console.WriteLine("The Average Age of these people is " + Person.SumOfAllAges / 4 + " years old.");
             System.Console.WriteLine();
-
-            //Invoke sum of all ages static method
-            //SumofAllAges();
-
+            
+            
             System.Console.WriteLine();
             System.Console.WriteLine("Select any key to complete...");
             System.Console.ReadKey();
