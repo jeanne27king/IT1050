b9998298bb7299c05c22fc4314ad3bfe7bde6058
jeanne27king <jeanne27king@att.net>
2016-02-10 21:02:27 -0500

    Update

---
+++ Lab2Solution/Lab2Person/Person.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab2Person
+{
+    class Person
+    {
+        //Member variable declaration
+        public int Age = 0;
+        public string firstName;
+        public string lastName;
+        public string spouseName;
+        public Person Spouse;
+
+        public void InputPersonData()        //invoke method input person firstName, LastName, and age?
+        {
+            System.Console.Write("What is your first name? ");
+            this.firstName = System.Console.ReadLine();
+            System.Console.Write("What is your last name? ");
+            this.lastName = System.Console.ReadLine();
+            System.Console.Write("What is your age? ");
+            this.Age = int.Parse(System.Console.ReadLine());
+            Age++;
+        }
+
+        public void SpouseInputData()  //invoke method input spouse name and age
+        {
+            System.Console.Write("What is your spouse's first name? ");
+            this.firstName = System.Console.ReadLine();
+            System.Console.Write("What is your spouse's age? ");
+            this.Age = int.Parse(System.Console.ReadLine());
+            Age++;     
+        }
+
+        public void PrintNameandAge()   //invoke print name and age?
+        {
+            System.Console.WriteLine(this.firstName + " " + this.lastName + " is " + this.Age + " years old.");
+        }
+
+
+        //public static int SumOfAllAges();
+        //    return Age/4;
+
+
+            
+
+    }
+}
---
+++ Lab2Solution/Lab2Person/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Lab2Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool maritalStatus;
+
+
+            //Person 1 
+            Person p1 = new Person();
+            System.Console.Write("Person 1: ");
+            //input person firstName, LastName, and age?
+            p1.InputPersonData();
+            System.Console.Write("True or False... Are you married? ");
+            maritalStatus = bool.Parse(System.Console.ReadLine());
+            //if marital status is true then create new spouse?
+            //input spouse firstName, set LastName, and age?
+            Person p1spouse = new Person();
+            p1spouse.SpouseInputData();
+            p1spouse.lastName = p1.lastName;
+            p1.spouseName = p1spouse.firstName;
+            p1spouse.spouseName = p1.firstName;
+            System.Console.WriteLine();
+
+
+            //Person 2
+            Person p2 = new Person();
+            System.Console.Write("Person 2: ");
+            //input person firstName, LastName, and age?
+            p2.InputPersonData();
+            System.Console.Write("True or False... Are you married? ");
+            maritalStatus = bool.Parse(System.Console.ReadLine());
+            //if marital status is true then create new spouse?
+            //input spouse firstName, set LastName, and age?
+            Person p2spouse = new Person();
+            p2spouse.SpouseInputData();
+            p2spouse.lastName = p2.lastName;
+            p2.spouseName = p2spouse.firstName;
+            p2spouse.spouseName = p2.firstName;
+            System.Console.WriteLine();
+
+            p1.PrintNameandAge();
+            p1spouse.PrintNameandAge();
+            p2.PrintNameandAge();
+            p2spouse.PrintNameandAge();
+            System.Console.WriteLine();
+
+            //Invoke sum of all ages static method
+            //SumofAllAges();
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Select any key to complete...");
+            System.Console.ReadKey();
+        }
+    }
+}
